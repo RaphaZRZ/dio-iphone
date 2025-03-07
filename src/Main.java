e889f9478a29946988299b09a7504f704d7d82b2
RaphaZRZ <raphazlinked@gmail.com>
2025-03-07 16:32:28 -0300

    feat: create Main.java and tested all iPhone methods

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+import cellphone.Iphone;
+
 public class Main {
     public static void main(String[] args) {
+        Iphone iphoneSeven = new Iphone();
 
+        System.out.println("1° iPhone status:");
+        iphoneSeven.showStatus();
+
+        System.out.println("===========================\n");
+
+        iphoneSeven.playMusic();
+        iphoneSeven.call();
+        iphoneSeven.connect();
+        iphoneSeven.browse();
+
+        System.out.println();
+
+        System.out.println("2° iPhone status:");
+        iphoneSeven.showStatus();
+
+        System.out.println("===========================\n");
+
+        iphoneSeven.pauseMusic();
+        iphoneSeven.endCall();
+        iphoneSeven.disconnect();
+        iphoneSeven.browse();
+
+        System.out.println();
+
+        System.out.println("3° iPhone status:");
+        iphoneSeven.showStatus();
     }
 }
